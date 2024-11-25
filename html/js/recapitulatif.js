2e7a2537aac2d8dcd121d2c76567f13493f9ae16
Belmond001 <kssbelastars@gmail.com>
2024-11-25 01:10:06 +0100

    update24_11_24

---
+++ html/js/recapitulatif.js
@@ -0,0 +1,3 @@
+$("#btn-valider-sondage").click(function(){
+    AfficherNotification("success","INFORMATION", "loader2", "Sondage publié avec succès", "OK", "javascript:gotoURL('./liste-sondage.html','loader')", "", "", "0");
+})
