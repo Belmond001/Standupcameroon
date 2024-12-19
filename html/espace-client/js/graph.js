53996e16b0bf526a5cc65d36ac5b867b6067863c
Belmond001 <kssbelastars@gmail.com>
2024-12-20 00:35:12 +0100

    update20_12_2024

--- html/espace-client/js/graph.js
+++ html/espace-client/js/graph.js
@@ -1,124 +1,51 @@
+// Nombre de questions
+const nb_question = document.querySelectorAll('.highcharts-figure').length;
 
+// Boucle pour chaque question
+for (let i = 1; i <= nb_question; i++) {
+    const containerId = 'container' + i;
+    const tableId = 'datatable' + i;
 
-
-
-// Grape 2
-
-Highcharts.chart('container2', {
-    data: {
-        table: 'datatable'
-    },
-    chart: {
-        type: 'column'
-    },
-    xAxis: {
-        type: 'category'
-    },
-    yAxis: {
-        allowDecimals: false,
-        title: {
-            text: 'Amount'
-        }
-    }
-});
-
-// Graph3
-
-// Data retrieved from https://www.ssb.no/en/transport-og-reiseliv/landtransport/statistikk/bilparken
-// Radialize the colors
-Highcharts.setOptions({
-    colors: Highcharts.map(Highcharts.getOptions().colors, function (color) {
-        return {
-            radialGradient: {
-                cx: 0.5,
-                cy: 0.3,
-                r: 0.7
+    // Vérifiez si le conteneur et la table existent avant de créer le graphique
+    if (document.getElementById(containerId) && document.getElementById(tableId)) {
+        Highcharts.chart(containerId, {
+            data: {
+                table: tableId
             },
-            stops: [
-                [0, color],
-                [1, Highcharts.color(color).brighten(-0.3).get('rgb')] // darken
-            ]
-        };
-    })
-});
-
-// Build the chart
-Highcharts.chart('container3', {
-    chart: {
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        type: 'pie'
-    },
-    tooltip: {
-        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-    },
-    accessibility: {
-        point: {
-            valueSuffix: '%'
-        }
-    },
-    plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
-                enabled: true,
-                format: '<span style="font-size: 1.2em"><b>{point.name}</b>' +
-                    '</span><br>' +
-                    '<span style="opacity: 0.6">{point.percentage:.1f} ' +
-                    '%</span>',
-                connectorColor: 'rgba(128,128,128,0.5)'
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: `Répartition des réponses relatives à la question ${i}`
+            },
+            accessibility: {
+                point: {
+                    valueSuffix: '%'
+                }
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                    },
+                    showInLegend: true
+                }
+            },
+            colors: ['#E37B03', '#50B432', '#FE2D01', '#F9C70C', '#491d05', '#8839B9', '#00AEEF'],
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.point.name + '</b><br/>' +
+                        this.point.y + ' voix';
+                }
             }
-        }
-    },
-    series: [{
-        name: 'Share',
-        data: [
-            { name: 'Candidat 1', y: 938899 },
-            { name: 'Candidat 2', y: 1229600 },
-            { name: 'Candidat 3', y: 325251 },
-            { name: 'Candidat 4', y: 238751 }
-        ]
-    }]
-});
-
-// Graphe 4
-
-Highcharts.chart('container4', {
-    chart: {
-        type: 'column'
-    },
-    xAxis: {
-        categories: ['Janvier', 'Fevrier', 'Mars', 'Octobre', 'Novembre', 'Décembre'],
-        crosshair: true,
-        accessibility: {
-            description: 'Countries'
-        }
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            text: 'Sondage'
-        }
-    },
-    tooltip: {
-        valueSuffix: ' (1000 MT)'
-    },
-    plotOptions: {
-        column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-        }
-    },
-    series: [
-        {
-            name: 'Homme',
-            data: [387749, 280000, 129000, 64300, 54000, 34300]
-        },
-        {
-            name: 'Femme',
-            data: [45321, 140000, 10000, 140500, 19500, 113500]
-        }
-    ]
-});
+        });
+    } else {
+        console.warn(`Le conteneur ou la table manquant(e) pour la question ${i}.`);
+    }
+}
